5f57708307a0770e97162e8449f6b87cf2ec988b
Guma-99 <skc06040@naver.com>
2022-08-03 10:40:31 +0900

    0803_STS3

---
+++ src/main/java/com/gm/start/test/DBconnectionTest.java
@@ -0,0 +1,10 @@
+package com.gm.start.test;
+
+public class DBconnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/gm/start/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.gm.start.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.println("책상_7 강의실");
+
+	}
+
+}
